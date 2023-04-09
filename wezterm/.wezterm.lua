228957e97a1288520fd8a722f20e3a2cac2531e7
Anthony Suárez <neolight1010@gmail.com>
2023-04-09 10:30:58 -0500

    wezterm: Import .wezterm.lua

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'tokyonight_night'
+config.window_decorations = 'RESIZE'
+
+return config
