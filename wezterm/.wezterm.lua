90db90c7c09ffdce9def9c079a5552be335dfac5
Anthony Suárez <neolight1010@gmail.com>
2023-09-03 08:46:16 -0500

    wezterm: Change font to Jetbrains

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.font = wezterm.font 'Lilex Nerd Font Mono'
+config.font = wezterm.font 'Jetbrains Mono'
 config.color_scheme = 'UnderTheSea'
 config.window_decorations = 'RESIZE'
 
