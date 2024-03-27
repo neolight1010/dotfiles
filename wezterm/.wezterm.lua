35bce24ffb29bb0d3e2085b6e7fef5221721a16a
Anthony Suárez <neolight1010@gmail.com>
2024-03-27 10:25:21 -0500

    wezterm: Change colorscheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'hardhacker'
+config.color_scheme = 'Horizon Dark (base16)'
 config.window_decorations = 'RESIZE'
 
 config.window_close_confirmation = 'NeverPrompt'
