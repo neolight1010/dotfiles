42eb6b1e43e57830c7f4fe4aded1160f9061871d
Anthony Suárez <neolight1010@gmail.com>
2024-07-10 16:13:20 -0500

    wezterm: Change colorscheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Kasugano (terminal.sexy)'
+config.color_scheme = 'X::DotShare (terminal.sexy)'
 config.window_decorations = 'RESIZE'
 config.font = wezterm.font("GohuFont uni14 Nerd Font Mono")
 config.font_size = 16
