917a1d1fca7e2cf4f3777791a877c9c580798e69
Anthony Suárez <neolight1010@gmail.com>
2024-09-24 14:32:49 -0500

    wezterm: Disable tabbar

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,8 +10,8 @@ config.color_scheme = 'X::DotShare (terminal.sexy)'
 config.window_decorations = 'RESIZE'
 config.font = wezterm.font("GohuFont uni14 Nerd Font Mono")
 config.font_size = 16
-
 config.window_close_confirmation = 'NeverPrompt'
 config.hide_mouse_cursor_when_typing = false
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
